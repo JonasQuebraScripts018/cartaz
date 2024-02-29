19b89ac81ee201a21702c2df88dfd11b5161113f
JonasQuebraScripts018 <gustavohpdelima@gmail.com>
2024-02-29 00:58:34 -0300

    00:57

---
+++ src/main/java/com/example/cartaz/CartazApplication.java
@@ -0,0 +1,13 @@
+package com.example.cartaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartazApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cartaz/CartazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cartaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
